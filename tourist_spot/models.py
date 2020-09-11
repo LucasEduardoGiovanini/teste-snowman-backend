7907bca0811f75b341a53ca7e8148d37ecfa4232
LucasEduardoGiovanini <39911940+LucasEduardoGiovanini@users.noreply.github.com>
2020-09-11 13:21:16 -0300

    criando a model básica de usuario e ponto turistico

--- tourist_spot/models.py
+++ tourist_spot/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class GerenciadorPerfilUsuario(BaseUserManager):
+    def criar_usuario(self, email, nome, senha):
+        email = self.normalize_email(email)
+        usuario = self.model(email, nome)
+
+        usuario.set_password(senha)  # encripta
+        usuario.save(using=self._db)
+        return usuario
+
+    def criar_superusuario(self, email, nome, senha):
+        super_usuario = self.criar_usuario(email, nome, senha)
+
+        super_usuario.is_superuser = True
+        super_usuario.is_staff = True
+        super_usuario.save(using=self._db)
+        return super_usuario
+
+
+class PerfilUsuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    nome = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    objects = GerenciadorPerfilUsuario()
+
+    def recupera_nome(self):
+        return self.nome
+
+    def __str__(self):
+        return self.email
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    latitude = models.FloatField(max_length=40)
+    longitude = models.FloatField(max_length=40)
+
+    categoria = models.ForeignKey(Categoria,
+                                  on_delete=models.CASCADE,
+                                  )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class GerenciadorPontoTuristico():
+    def consulta_categoria(self, categoria: str):
+        categoria_existente = PontoTuristico().objects.get(categoria=categoria.upper())
+        return categoria_existente if categoria_existente else False
+
+    def consulta_ponto_turistico(self, ponto_turistico: PontoTuristico.nome):
+        ponto_turistico = PontoTuristico().objects.get(name=ponto_turistico.upper())
+        return ponto_turistico if ponto_turistico else False
